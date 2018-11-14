a774060ed83b4f3872ae508669a0cafdd7053513
Kuras <lukevito@gmail.com>
2018-11-14 23:50:22 +0100

    init

---
+++ src/main/java/com/gmail/lukevito/GreetingController.java
@@ -0,0 +1,15 @@
+package com.gmail.lukevito;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "page";
+    }
+}
